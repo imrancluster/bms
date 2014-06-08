68c2535cbf9d85ae909166c909401cfe396d1d79
Imran Cluster <imrancluster@gmail.com>
2014-06-09 00:29:02 +0600

    create demo home page using twitter bootstrap 3

--- src/BMS/PublicBundle/Controller/DefaultController.php
+++ src/BMS/PublicBundle/Controller/DefaultController.php
@@ -6,8 +6,26 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class DefaultController extends Controller
 {
-    public function indexAction($name)
+    public function indexAction()
     {
-        return $this->render('PublicBundle:Default:index.html.twig', array('name' => $name));
+        return $this->render('PublicBundle:Default:index.html.twig');
+    }
+
+    public function descriptionAction()
+    {
+    	$data = "Description Page";
+
+    	return $this->render('PublicBundle:Default:description.html.twig', array(
+    		'data' => $data
+    	));
+    }
+
+    public function contactAction()
+    {
+    	$data = "Contact Page";
+
+    	return $this->render('PublicBundle:Default:contact.html.twig', array(
+    		'contact' => $data
+    	));
     }
 }
